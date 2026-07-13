f85bc7bd7810a2d95825ff3519912b7e509b1160
souliya_pps <souliya.litd@gmail.com>
2026-07-13 13:30:18 +0700

    fix: move mobile bulk bar outside table element for valid HTML nesting

--- views/pages/admin/sales/index.php
+++ views/pages/admin/sales/index.php
@@ -156,6 +156,7 @@
                         </tr>
                     </tfoot>
                     <?php endif; ?>
+                </table>
 
             <!-- Mobile Bulk Action Bar -->
             <div x-show="selected.length > 0" style="display: none" class="fixed bottom-0 left-0 right-0 bg-card border-t shadow-2xl px-4 py-3 z-50 md:hidden">
@@ -169,7 +170,6 @@
                     </button>
                 </div>
             </div>
-                </table>
             </div>
         </div>
     </div>
